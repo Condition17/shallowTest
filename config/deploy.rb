b06b3ef33afe86bf287f3c03f41e240758ea13bd
Cristian Conache <cconache@gmail.com>
2017-04-23 13:06:00 +0300

    Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,21 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :user, "vlad"
+
+set :application, 'shallowTest'
+set :repo_url, 'git@github.com:Condition17/shallowTest.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+ set :deploy_to, '/home/vlad/shallowTest'
 
 # Default value for :scm is :git
-# set :scm, :git
+
+set :scm, :git
+set :branch, "master"
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -33,6 +37,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+# set :rails_env, "production"
+
+# set :ssh_options, { :forward_agent => true, :port => 22 }
+# default_run_options[:pty] = true
 
 namespace :deploy do
 
