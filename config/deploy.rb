78d79255de295e23999387db3e8ba9dc2282ff3e
Vlad Verestiuc <verestiuc.vlad@gmail.com>
2017-04-25 18:25:26 +0300

    Make deployment grate again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,33 @@
 # config valid only for current version of Capistrano
 
-set :user, "vlad"
-
-set :application, 'shallowTest'
+set :application, 'shallow_test'
 set :repo_url, 'git@github.com:Condition17/shallowTest.git'
+set :branch, "rails-app"
 
+# Do not change afer this line
+
+set :user, "vlad"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.0'
 
-# Default branch is :master
-set :branch, "rails-app"
-
-set :deploy_to, "/home/vlad/#{fetch(:application)}"
+set :deploy_to, "/srv/apps/#{fetch(:application)}"
 
 set :puma_init_active_record, true
-# set :format, :pretty
 set :log_level, :debug
 set :pty, true
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+set :default_env, { path: "$HOME/.rbenv/shims/:$PATH" }
 set :rails_env, "production"
-
-
 set :ssh_options, { forward_agent: true, port: 22, paranoid: false}
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 
-namespace :deploy do
+namespace :puma do
 
-  after :restart, :clear_cache do
+  after :nginx_config, :reload_nginx_config do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      sudo :nginx, '-s', 'reload'
     end
   end
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
 set :stage, :staging
-server "89.36.26.172", user: 'vlad', roles: %w{web app db}, primary: true
+set :nginx_server_name, "staging-af0f.89.36.26.172.xip.io"
+server "89.36.26.172", user: fetch(:user) , roles: %w{web app db}, primary: true
 
